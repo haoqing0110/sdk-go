f1545efee773765bcea435a70f8b7fd5e54a10ef
Wei Liu <liuweixa@redhat.com>
2025-03-27 17:19:09 +0800

    add list metadata to store list (#104)
    Signed-off-by: Wei Liu <liuweixa@redhat.com>
    Signed-off-by: Qing Hao <qhao@redhat.com>

---
+++ pkg/cel/common/values.go
@@ -0,0 +1,19 @@
+package common
+
+import (
+	"reflect"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+func ConvertObjectToUnstructured(obj interface{}) (*unstructured.Unstructured, error) {
+	if obj == nil || reflect.ValueOf(obj).IsNil() {
+		return &unstructured.Unstructured{Object: nil}, nil
+	}
+	ret, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
+	if err != nil {
+		return nil, err
+	}
+	return &unstructured.Unstructured{Object: ret}, nil
+}
